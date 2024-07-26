05cc8a60c2aa7257b76d0eda2ecc1f142e196a31
faisalmujawar148 <faisal.mujawar4@gmail.com>
2024-07-27 00:46:52 +0100

    updated

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,10 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
+import vercel from "@astrojs/vercel/serverless";
 
 export default defineConfig({
-  site: "https://astro-nano-demo.vercel.app",
+  output: 'server',
+  adapter: vercel(),
   integrations: [mdx(), sitemap(), tailwind()],
 });
